29c599a1fb3db873becc5777de5e378286626a71
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2019-09-18 21:45:29 -0300

    create product page

--- src/view/pages/AddProduct/index.js
+++ src/view/pages/AddProduct/index.js
@@ -1,7 +1,50 @@
 import React from "react";
 
 const AddProduct = () => {
-  return <div>AddProduct</div>;
+  return (
+    <div className="row">
+      <form className="col s12 m6 offset-m3">
+        <div className="row">
+          <div className="input-field col s12">
+            <input id="name" type="text" />
+            <label htmlFor="name">Name</label>
+          </div>
+        </div>
+        <div className="row">
+          <div className="input-field col s12">
+            <input id="prize" type="number" />
+            <label htmlFor="prize">Prize</label>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="file-field input-field">
+            <div className="btn">
+              <span>Image</span>
+              <input type="file" multiple />
+            </div>
+            <div className="file-path-wrapper">
+              <input
+                className="file-path validate"
+                type="text"
+                placeholder="Set a cool image to you product"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="input-field col s12 offset-m3">
+            <button
+              className="btn waves-effect waves-light deep-purple"
+              type="submit"
+            >
+              Create Product
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default AddProduct;
