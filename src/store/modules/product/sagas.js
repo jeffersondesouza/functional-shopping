8f97ca5c364dd3caf16894d5f54dfd8a644fa49b
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2019-09-21 15:49:21 -0300

    saga ofl oad ok

--- src/store/modules/product/sagas.js
+++ src/store/modules/product/sagas.js
@@ -9,14 +9,23 @@ import productListFactory from "../../../domain/factories/productListFactory";
 import { loadProductsQuery } from "../../../domain/repositories/ProductRepository";
 
 function* loadProductsEffect({ payload }) {
-  
-  const { data } = yield call(httpFetch.request, loadProductsQuery);
+  try {
+    const { data } = yield call(httpFetch.request, loadProductsQuery);
 
-  const products = productListFactory(data.products);
+    const products = productListFactory(data.products);
 
-  yield put(actions.updateProducts(products));
-  yield put(actions.updateProductsCounter(data.count));
-  yield put(actions.loadProductSuccess());
+    yield put(actions.updateProducts(products));
+    yield put(actions.updateProductsCounter(data.count));
+    yield put(actions.loadProductSuccess());
+  } catch (error) {
+    yield put(
+      actions.loadProductFailure({
+        msg: "Não foi possível carrregar os produtos",
+        data: { ...error },
+        hasError: true
+      })
+    );
+  }
 }
 
 function* watchLoadProduct() {
