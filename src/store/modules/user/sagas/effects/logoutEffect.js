0bae339e134296f998bfa4a13dcf06d8761d3cbd
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2019-09-21 11:23:42 -0300

    logout oko

--- src/store/modules/user/sagas/effects/logoutEffect.js
+++ src/store/modules/user/sagas/effects/logoutEffect.js
@@ -1,27 +1,14 @@
-import { put, call } from "redux-saga/effects";
+import { put } from "redux-saga/effects";
 
 import actions from "../../actions";
 
-import pipe from "../../../../../utils/functions/pipe";
-import httpFetch from "../../../../../domain/services/httpFetch";
-import { loginQuery } from "../../../../../domain/repositories/UserRepository";
-import { buildUser } from "../../../../../domain/factories/UserFactory";
-
 function* logoutEffect({ payload }) {
   try {
-    const { data } = yield call(httpFetch.request, loginQuery, payload);
-
-    yield put(
-      pipe(
-        buildUser,
-        actions.updateUser
-      )(data)
-    );
-
-    yield put(actions.loginSuccess());
+    yield put(actions.logoutSuccess());
+    window.location.reload();
   } catch (error) {
     yield put(
-      actions.loginFailure({
+      actions.logoutFailure({
         data: error,
         msg: "Could Not login",
         hasError: true
