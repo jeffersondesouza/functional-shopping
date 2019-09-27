3a6388b0e750969f013eaf5d04c22afc96f731a5
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2019-09-27 11:12:00 -0300

    Router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import "./styles/index.scss";
 import App from "./App";
@@ -13,9 +13,9 @@ const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </Provider>,
   document.getElementById("root")
 );
